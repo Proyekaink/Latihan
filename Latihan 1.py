16774165610d1dae451d8d50a363b0752d77b71d
Rama Aditra <ramaaditra29@gmail.com>
2021-03-18 23:23:42 +0700

    Percobaan pertama

---
+++ Latihan 1.py
@@ -0,0 +1,18 @@
+print('Halo Dunia Tiputipu')
+print('by Rama')
+print('tanggal 18 maret 2021')
+print('-'*10)
+print('ingin lulus lama')
+
+ingin_lulus_cepat = False
+if ingin_lulus_cepat:
+    print('kuliah yang bener tot')
+else:
+    print('calon donatur kampus')
+print('-'*10)
+
+jumlah_anak = 3
+
+for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
+    print(f'Halo anak #{index_anak}')
+
